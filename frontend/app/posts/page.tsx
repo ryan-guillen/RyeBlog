dab768a059e31cd5fd13e6b8bb90111a37924316
Ryan Guillen <ryantguillen@gmail.com>
2022-11-22 13:50:02 -0500

    removed backend

--- frontend/app/posts/page.tsx
+++ frontend/app/posts/page.tsx
@@ -1,7 +1,43 @@
-const Posts = () => {
+const apiCall = async () => {
+    
+    const res = await fetch(`http://localhost:8080/product/637d15cd0fb56203d652363d`, { // add
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        //body: JSON.stringify({'name': 'Product 2', 'description': 'Product 2 description'})
+    });
+    
+   /*
+    const res = await fetch(`http://localhost:8080/employees/4`, { // replace / update
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'name': 'Samwise Gamgee', 'role': 'gardener'})
+    });
+    */
+   /*
+    const res = await fetch(`http://localhost:8080/employees/4`, { // replace / update
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+    */
+    const data = await res.json();
+    console.log(data);
+    return data;
+}
+
+
+const Posts = async () => {
+    const r = await apiCall();
+
     return (
         <div>
             In Posts
+            <button>call API</button>
         </div>
     )
 }
